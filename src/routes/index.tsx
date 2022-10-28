820a8585093bfe544261d0de1d882bc72f7b5a1e
alebandieragodoi <alexandre@dobesone.com.br>
2022-10-28 18:05:22 -0300

    Feat: Adicionar biblioteca da material ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route
+        path="/pagina-inicial"
+        element={<Button>Meu primeiro Button da MUI</Button>}
+      />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
