6ef3257fa8dd353197d02744ad76ffe3383766bd
alebandieragodoi <alexandre@dobesone.com.br>
2022-10-31 17:56:35 -0300

    Feat: Adionando thema escuro e troca de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
-            Teste
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Trocar tema
           </Button>
         }
       />
---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+import { cyan, yellow } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+  palette: {
+    primary: {
+      light: yellow[500],
+      main: yellow[700],
+      dark: yellow[800],
+
+      contrastText: "#fffffff",
+    },
+
+    secondary: {
+      main: cyan[500],
+      dark: cyan[400],
+      light: cyan[300],
+      contrastText: "#fffffff",
+    },
+
+    background: {
+      paper: "#303134",
+      default: "#202124",
+    },
+  },
+});
