c790311fca41436e17702ecd6d3238b3d8dd9376
alebandieragodoi <alexandre@dobesone.com.br>
2022-11-08 18:13:16 -0300

    Feat: Layout base responsivo

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,12 @@
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const DashBoard = () => {
-  return <LayoutBaseDePagina titulo="Página inicial">ABC</LayoutBaseDePagina>;
+  return (
+    <LayoutBaseDePagina
+      titulo="Página inicial"
+      barraDeFerramentas={<>Barra de ferramentas</>}
+    >
+      Testando
+    </LayoutBaseDePagina>
+  );
 };
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -6,19 +6,23 @@ import {
   useTheme,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import { ReactNode } from "react";
 import { useDrawerContext } from "../contexts";
 
 interface ILayoutBaseDePaginaProps {
   children: React.ReactNode;
   titulo: string;
+  barraDeFerramentas?: ReactNode;
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
   children,
   titulo,
+  barraDeFerramentas,
 }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery(theme.breakpoints.down("md"));
 
   const { toggleDrawerOpen } = useDrawerContext();
 
@@ -26,7 +30,7 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
     <Box height={"100%"} display="flex" flexDirection={"column"} gap={1}>
       <Box
         padding={1}
-        height={theme.spacing(12)}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
         display="flex"
         alignItems={"center"}
       >
@@ -36,12 +40,21 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
           </IconButton>
         )}
 
-        <Typography variant="h5">{titulo}</Typography>
+        <Typography
+          variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+          whiteSpace={"nowrap"}
+          overflow="hidden"
+          textOverflow={"ellipses"}
+        >
+          {titulo}
+        </Typography>
       </Box>
 
-      <Box>Barra de ferramentas</Box>
+      {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
 
-      <Box>{children}</Box>
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
