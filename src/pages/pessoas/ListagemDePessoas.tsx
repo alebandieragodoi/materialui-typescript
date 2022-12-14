74811b0a1f975c5e11ef3c4882c4cf27cb3f2a09
alebandieragodoi <alexandre@dobesone.com.br>
2022-12-14 16:46:39 -0300

    Feat: Evitar consultas desnecessárias useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
+import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const { debounce } = useDebounce(3000);
+
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PessoasService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message);
-        return;
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      PessoasService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+          return;
+        } else {
+          console.log(result);
+        }
+      });
     });
   }, [busca]);
 
